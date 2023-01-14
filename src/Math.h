d6362ba054f0fdd96335ac87543843a5ea2859c4
Hemmy <thehemmy123@gmail.com>
2023-01-14 17:20:32 +0000

    Messing about with a generic Vector class

--- src/Math.h
+++ src/Math.h
@@ -8,15 +8,37 @@
 #include <cmath>
 #include <iostream>
 #include <array>
-// Template vector clas that defaults to 3
+
+
+
+
+// Template vector class that defaults to float and using 3 values and
 template<class Type = float , int SIZE = 3>
 struct Vector
 {
-    std::array<Type,SIZE> elems[SIZE]{};
+	
+	constexpr Vector(){
+		// we default to a size of 3, so we manually set the values here.
+		std::fill(e.begin(),e.end(),0);
+	}
+
+	// Make a vector with any number of components.
+	template<class ... Components>
+	explicit Vector(Components... elems)
+	{
+		e = std::array<Type, sizeof...(elems)>(elems...);
+	}
+	
+	std::array<Type,SIZE> e;
+	
+	Type x() const {return e.at(0);};
+	Type y() const {return e.at(1);};
+	Type z() const {return e.at(2);};
+
+	// enable only if size is >= 4
+	Type w() const requires (SIZE >= 4) {return e.at(3);};
 
 
 
 };
-
-
 #endif //RAYTRACING_WEEKEND_MATH_H
--- src/main.cpp
+++ src/main.cpp
@@ -1,34 +1,57 @@
 #include <iostream>
 #include <fstream>
+#include <vector>
+#include "Math.h"
 
+void PrintImage()
+{
+	// Image
+	std::ofstream outputFile("image.ppm");
+	std::cout.rdbuf(outputFile.rdbuf()); //redirect std::cout to out.txt!
+
+	const int image_width = 1920	;
+	const int image_height = 1080;
+
+	// Render
+
+	std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+
+	for (int j = image_height-1; j >= 0; --j) {
+		std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
+
+		for (int i = 0; i < image_width; ++i) {
+
+			auto r = double(i) / (image_width-1);
+			auto g = double(j) / (image_height-1);
+			auto b = 0.25;
+
+			int ir = static_cast<int>(255.999 * r);
+			int ig = static_cast<int>(255.999 * g);
+			int ib = static_cast<int>(255.999 * b);
+
+			std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+		}
+	}
+	std::cerr << "\nDone.\n";
+}
+
+void VectorTest()
+{
+	using namespace std;
+	Vector temp;
+
+	std::vector<int> thing;
+	Vector<float, 5> temp2;
+
+	std::cout << temp.x() << endl;
+	std::cout << temp.y() << endl;
+	std::cout << temp2.w() << endl;
+
+}
 int main() {
+	VectorTest();
 
-    // Image
-    std::ofstream outputFile("image.ppm");
-    std::cout.rdbuf(outputFile.rdbuf()); //redirect std::cout to out.txt!
 
-    const int image_width = 256;
-    const int image_height = 256;
 
-    // Render
 
-    std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
-
-    for (int j = image_height-1; j >= 0; --j) {
-        std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
-
-        for (int i = 0; i < image_width; ++i) {
-
-            auto r = double(i) / (image_width-1);
-            auto g = double(j) / (image_height-1);
-            auto b = 0.25;
-
-            int ir = static_cast<int>(255.999 * r);
-            int ig = static_cast<int>(255.999 * g);
-            int ib = static_cast<int>(255.999 * b);
-
-            std::cout << ir << ' ' << ig << ' ' << ib << '\n';
-        }
-    }
-    std::cerr << "\nDone.\n";
 }
